a94ea4b0ec8795c7b38642050aad351ca5f73170
Ian Koh <ian_koh93@hotmail.com>
2022-03-11 19:10:29 -0800

    added a new test branch and learning about errors and exceptions

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
 
-import pandas
+# Testing File not found exceptions
 
-data = pandas.read_csv("nato_phonetic_alphabet.csv")
-#TODO 1. Create a dictionary in this format:
-phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(phonetic_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["asdsa"])
+except FileNotFoundError:
+    # Put a good alternative like creating the file from scratch
+    open("a_file.txt", "w")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist")
